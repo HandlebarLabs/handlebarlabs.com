ead25b5cf0db3d6ce8a490096c1c43c778da31de
Spencer Carli <spencercarli@gmail.com>
2014-08-31 14:50:14 -0400

    Setting up the child theme

---
+++ themes/handlebarlabs/functions.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Functions
+ *
+ * @package      HBL_Genesis_Child
+ * @since        1.0.0
+ * @link         https://github.com/billerickson/BE-Genesis-Child
+ * @author       Spencer Carli <spencercarli@gmail.com>
+ *
+ */
+
+/**
+ * Theme Setup
+ * @since 1.0.0
+ *
+ * This setup function attaches all of the site-wide functions
+ * to the correct hooks and filters. All the functions themselves
+ * are defined below this setup function.
+ *
+ */
+
+add_action('genesis_setup','child_theme_setup', 15);
+function child_theme_setup() {
+
+}
