90bd15e20d505d458b58423b62bd27a5ba5b6229
Spencer Carli <spencercarli@gmail.com>
2014-09-12 12:16:44 -0400

    Working on copy

--- themes/handlebarlabs/pages/foodtruck.php
+++ themes/handlebarlabs/pages/foodtruck.php
@@ -32,7 +32,7 @@ Template Name: Food Truck Landing Page
         <h2>Great on Any Device</h2>
         <p>
           Don't worry about where your customer is browsing from. Your website will look great on any device. Anytime, from anywhere.
-          At Handlebar Labs we use responsive design to build one site that will work on any size device.
+          At Handlebar Labs we use responsive design to build one website for your food truck that will work on any size device.
         </p>
       </div>
       <div class="col-md-4">
@@ -51,7 +51,8 @@ Template Name: Food Truck Landing Page
       <div class="col-md-8 col-md-push-4">
         <h2>Easy Calendar Integration</h2>
         <p>
-          Keep you loyal fans in the know. Provide an easy to follow and always up to date calendar right on your homepage.
+          Keep you loyal fans in the know and inform your soon to be fans where you're going to be.
+          We provide an easy to follow and always up to date calendar right on your food truck's website.
           No longer will your customers have to search through dozens of tweets to find out where you are.
         </p>
       </div>
@@ -71,8 +72,8 @@ Template Name: Food Truck Landing Page
       <div class="col-md-8">
         <h2>Amazing Menu</h2>
         <p>
-          Once your customers know where you are, let them know what they want. Provide a full featured menu for your customers to love.
-          Build up the burning desire to come out to your truck at lunch. Play on that primal instict, give them food.
+          Once your customers know where you are, let them know exactly what you've got. Provide a full featured menu for your customers to love.
+          To build up a burning craving to dash out of the office at lunch to come visit you.
         </p>
       </div>
       <div class="col-md-4">
@@ -88,13 +89,21 @@ Template Name: Food Truck Landing Page
   <!-- Pricing -->
   <div class="row pricing section">
     <div class="container">
+
       <div class="col-md-12">
-        <h2>Starting at only $500</h2>
+        <h2>Are you ready?<br> Pricing starts at only $500</h2>
         <p>
           +$35/month for premium web hosting
         </p>
         <a href="#" class="btn btn-primary">Get a Free Consultation</a>
       </div>
+
+      <div class="col-md-8 col-md-offset-2">
+        <p>
+          Don't worry. You're under <b>zero</b> obligation to buy. We'll talk with you and figure out exactly what is right for your food truck.
+        </p>
+      </div>
+
     </div>
   </div>
 
