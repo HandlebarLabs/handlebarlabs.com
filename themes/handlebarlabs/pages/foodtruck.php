bbce734c3f5cd668ab26d2435009b0979ee4671d
Spencer Carli <spencercarli@gmail.com>
2014-09-12 11:08:44 -0400

    Added food truck sections and some content

--- themes/handlebarlabs/pages/foodtruck.php
+++ themes/handlebarlabs/pages/foodtruck.php
@@ -16,11 +16,54 @@ Template Name: Food Truck Landing Page
 </style>
 
 <div class="foodtruck">
+
   <!-- Leader -->
   <div class="row leader">
     <div class="container">
       <h1><span>Simple and Beautiful Design <span class="spacer"></span> <br>
         <span class="spacer"></span> for your Food Truck's Website</span></h1>
+    </div>
+  </div>
+
+  <!-- Responsive Section -->
+  <div class="row responive">
+    <div class="container">
+      <div class="col-md-8">
+        <h2>Great on Any Device</h2>
+        <p>
+          Don't worry about where your customer is browsing from. Your website will look great on any device. Anytime, from anywhere.
+          At Handlebar Labs we use responsive design to build one site that will work on any size device.
+        </p>
+      </div>
+      <div class="col-md-4"></div>
+    </div>
+  </div>
+
+  <!-- Calendar Section -->
+  <div class="row calendar">
+    <div class="container">
+      <div class="col-md-8 col-md-push-4">
+        <h2>Easy Calendar Integration</h2>
+        <p>
+          Keep you loyal fans in the know. Provide an easy to follow and always up to date calendar right on your homepage.
+          No longer will your customers have to search through dozens of tweets to find out where you are.
+        </p>
+      </div>
+      <div class="col-md-4 col-md-pull-8"></div>
+    </div>
+  </div>
+
+  <!-- Menu Section -->
+  <div class="row menu">
+    <div class="container">
+      <div class="col-md-8">
+        <h2>Amazing Menu</h2>
+        <p>
+          Once your customers know where you are, let them know what they want. Provide a full featured menu for your customers to love.
+          Build up the burning desire to come out to your truck at lunch. Play on that primal instict, give them food.
+        </p>
+      </div>
+      <div class="col-md-4"></div>
     </div>
   </div>
 
@@ -30,4 +73,5 @@ Template Name: Food Truck Landing Page
       <?php hbl_do_footer(); ?>
     </div>
   </footer>
+
 </div>
