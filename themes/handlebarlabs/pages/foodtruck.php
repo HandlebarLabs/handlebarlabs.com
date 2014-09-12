5e61a18dd23791d240a425960e7ae3dae5bc75d2
Spencer Carli <spencercarli@gmail.com>
2014-09-12 10:44:08 -0400

    Added a header for food truck landing

--- themes/handlebarlabs/pages/foodtruck.php
+++ themes/handlebarlabs/pages/foodtruck.php
@@ -15,7 +15,19 @@ Template Name: Food Truck Landing Page
 .site-inner {display: none;}
 </style>
 
-<!-- Leader -->
-<div class="row leader">
+<div class="foodtruck">
+  <!-- Leader -->
+  <div class="row leader">
+    <div class="container">
+      <h1>Simple and Beautiful Design<br>
+        for your Food Truck's Website</h1>
+    </div>
+  </div>
 
+  <!-- Footer -->
+  <footer class="site-footer" role="contentinfo" itemscope="itemscope" itemtype="http://schema.org/WPFooter">
+    <div class="wrap">
+      <?php hbl_do_footer(); ?>
+    </div>
+  </footer>
 </div>
