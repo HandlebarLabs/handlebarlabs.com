77ddaf6b3f0d0e559d283d3622677e3e1400b13b
Spencer Carli <spencercarli@gmail.com>
2014-09-12 13:08:58 -0400

    Add shortcode for contact form

--- themes/handlebarlabs/pages/foodtruck.php
+++ themes/handlebarlabs/pages/foodtruck.php
@@ -119,22 +119,7 @@ Template Name: Food Truck Landing Page
   <div class="container contact">
     <div class="row">
       <div class="col-md-6 col-md-offset-3">
-        <form role="form">
-          <div class="form-group">
-            <label for="exampleInputEmail1">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
-          </div>
-          <div class="form-group">
-            <label for="exampleInputPassword1">Password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-          </div>
-          <div class="checkbox">
-            <label>
-              <input type="checkbox"> Check me out
-            </label>
-          </div>
-          <button type="submit" class="btn btn-default">Submit</button>
-        </form>
+        <?php echo do_shortcode( '[contact-form-7 id="2245" title="Food Truck Landing"]' ); ?>
       </div>
     </div>
   </div>
