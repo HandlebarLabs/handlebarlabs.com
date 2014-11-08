785aea27ae7c5c4c5b1c66cf4c588eb26ac7357b
Spencer Carli <spencercarli@gmail.com>
2014-11-08 17:03:23 -0500

    Use an overlay contact window

--- _site/assets/js/hbl.js
+++ _site/assets/js/hbl.js
@@ -11,6 +11,8 @@ $(function() {
     return this;
   };
 
+  // Homepage
+  // =========================================
   // Center hero unit
   $('.hero').center();
 
@@ -18,4 +20,37 @@ $(function() {
   $(window).resize(function() {
     $('.hero').center();
   });
+
+
+  // Contact
+  // =========================================
+  $('#contact').on('click', function(e) {
+    e.preventDefault();
+
+    var docHeight = $(document).height();
+    $("body").append(
+      "<div id='contactOverlay' class='contact-overlay'>" +
+        "<span class='close pull-right'><i class='fa fa-close fa-2x'></i></span>" +
+        "<div class='contact-text'>" +
+          "<h1>Let's talk.</h1> <h3>Whether you want to talk or just ask us a question, " +
+          "we want to hear from you.</h3>" +
+          "<h3>Send an email to <a href='mailto:hello@handlebarlabs.com' target='_blank'>" +
+          "hello@handlebarlabs.com</a></h3>" +
+        "</div>" +
+      "</div>"
+    );
+
+    var overlay = $("#contactOverlay");
+    overlay.height(docHeight);
+    overlay.find('.contact-text').center();
+
+    $('#contactOverlay').on('click', function(e) {
+      $('#contactOverlay').remove();
+    });
+
+    $(window).resize(function() {
+      overlay.find('.contact-text').center();
+    });
+  });
+
 });
