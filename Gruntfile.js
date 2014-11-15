4b69dc9106dbd437582794fa6f2bbe44abaadbe8
Spencer Carli <spencercarli@gmail.com>
2014-11-15 18:01:44 -0500

    Use grunt for less files

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,30 @@
 module.exports = function(grunt) {
-  
+
   // Config
   grunt.initConfig({
     shell: {
       serve: {
         command: 'jekyll serve -w'
       }
+    },
+
+    less: {
+      development: {
+        options: {
+          paths: ['assets/css'],
+          cleancss: true
+        },
+        files: {
+          "assets/css/style.css" : "assets/css/style.less"
+        }
+      }
     }
   });
 
   // Loan plugins
   grunt.loadNpmTasks('grunt-shell');
+  grunt.loadNpmTasks('grunt-contrib-less');
 
   // Register Tasks
-  grunt.registerTask('default', ['shell:serve']);
+  grunt.registerTask('default', ['less:development','shell:serve']);
 };
