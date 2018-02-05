3c030f055ddf9de782497b12e9af7f145cd6f7dc
Spencer Carli <spencercarli@gmail.com>
2018-02-05 16:26:55 -0600

    Toying around with styled-components

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -6,35 +6,6 @@ import Helmet from 'react-helmet';
 import './index.css';
 import './custom.css';
 
-const Header = () => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          Gatsby
-        </Link>
-      </h1>
-    </div>
-  </div>
-);
-
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet
