b41979215cc965091b7f4547a9141325a14b8c08
Spencer Carli <spencercarli@gmail.com>
2018-04-17 11:48:24 -0500

    Fix

--- src/pages/m/react-native-push-notifications.js
+++ src/pages/m/react-native-push-notifications.js
@@ -4,7 +4,7 @@ import Script from 'react-load-script';
 const SecondPage = () => (
   <div className="email-list-background">
     <div className="email-list-content">
-      <div id="_ck_362134" />
+      <div id="_ck_376656" />
       <Script url="https://forms.convertkit.com/376656?v=7" />
     </div>
   </div>
