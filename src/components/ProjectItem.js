5fc6419e8409b7198535e5ec83d1bd81d1eebc94
Spencer Carli <spencercarli@gmail.com>
2021-05-24 15:59:26 -0500

    Alignment

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -3,11 +3,11 @@ import { Link } from 'gatsby';
 
 const ProjectItem = ({ slug, imageUrl, name, caption }) => (
   <Link to={slug}>
-    <div className="mb-10 flex flex-row items-center">
+    <div className="mb-10 flex flex-col md:flex-row md:items-center">
       {imageUrl && (
         <img
           src={imageUrl}
-          className="w-24 h-24 object-cover rounded mr-6"
+          className="w-24 h-24 object-cover rounded mr-6 mb-2 md:mb-0"
           alt={`${name} Logo`}
         />
       )}
