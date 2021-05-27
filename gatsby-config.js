b9501e1f17cd0f5d200f560e32c042b85d80ea1d
Spencer Carli <spencercarli@gmail.com>
2021-05-27 16:06:39 -0500

    Fix google analytics and sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,22 +40,9 @@ module.exports = {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
-        trackingIds: ['UA-45179732-1'],
-        // This object gets passed directly to the gtag config command
-        // This config will be shared across all trackingIds
-        gtagConfig: {
-          optimize_id: 'OPT_CONTAINER_ID',
-          anonymize_ip: true,
-          cookie_expires: 0,
-        },
-        // This object is used for configuration specific to this plugin
+        trackingIds: ['UA-45179732-1', 'G-NYSJ9NH8JP'],
         pluginConfig: {
-          // Puts tracking script in the head instead of the body
-          head: false,
-          // Setting this parameter is also optional
           respectDNT: true,
-          // Avoids sending pageview hits from custom paths
-          exclude: [],
         },
       },
     },
